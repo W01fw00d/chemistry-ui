71d33f810f0f03ac807ca54ed3b128122ed14822
Gabriel Romay Machado <romay.gabriel@gmail.com>
2020-04-14 19:39:49 +0900

    Fix issues with webpack; update version

--- src/templates/index.js
+++
@@ -1,2 +0,0 @@
-export { default as RoomListTemplate } from './roomListTemplate';
-export { default as RoomDetailTemplate } from './roomDetailTemplate';
--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: [
           {
             loader: 'babel-loader',
             options: {
-              presets: ['preset-env', 'react'],
+              presets: ['@babel/preset-env', '@babel/preset-react'],
             },
           },
         ],
