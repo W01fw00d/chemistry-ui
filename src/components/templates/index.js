96d4e8b066189eeff6941462d8b78a5c6b4ee851
Gabriel Romay Machado <romay.gabriel@gmail.com>
2020-04-27 18:35:33 +0900

    ADd new templates to index.js

--- src/components/templates/index.js
+++ src/components/templates/index.js
@@ -1,2 +1,4 @@
 export { default as ItemListTemplate } from './itemListTemplate.jsx';
+export { default as RecipeListTemplate } from './recipeListTemplate.jsx';
 export { default as ItemDetailTemplate } from './itemDetailTemplate.jsx';
+export { default as RecipeDetailTemplate } from './recipeDetailTemplate.jsx';
