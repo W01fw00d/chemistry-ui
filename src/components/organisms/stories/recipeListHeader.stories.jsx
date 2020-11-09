93732570ad96045c139ab2c4f56211f0c011f06c
Gabriel Romay Machado <romay.gabriel@gmail.com>
2020-11-09 23:04:23 +0900

    Fix missing literals on story

--- src/components/organisms/stories/recipeListHeader.stories.jsx
+++ src/components/organisms/stories/recipeListHeader.stories.jsx
@@ -4,6 +4,8 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import theme from '../../../styles/global-styles';
+
+import allLiterals from '../../../../.storybook/fake_data/literals.json';
 import themeDecorator from '../../../../.storybook/decorators/themeDecorator';
 import authorData from '../../../../.storybook/fake_data/author.json';
 import projectData from '../../../../.storybook/fake_data/project.json';
@@ -22,5 +24,6 @@ storiesOf('Organisms/Recipe/List/Header', module)
       searchValue="Search Value"
       handleChange={action('Input detected')}
       handleClick={action('IconButton clicked')}
+      literals={allLiterals}
     />
   ));
