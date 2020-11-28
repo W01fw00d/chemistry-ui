6f193eabd3e2df6bbede7e9a55d3d94778d73b7b
Gabriel Romay Machado <romay.gabriel@gmail.com>
2020-11-28 13:41:07 +0900

    Implement alternative feature state preserved only while uncollapsed

--- src/components/organisms/recipe/detail/ingredientsList/components/item.jsx
+++ src/components/organisms/recipe/detail/ingredientsList/components/item.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { ListItemSecondaryAction as Action } from '@material-ui/core';
@@ -9,15 +9,30 @@ import Item from '../../../../list/item/index.jsx';
 import Checkbox from '../../../../list/item/checkbox.jsx';
 
 const Component = ({ name, emoji, quantity, isOptional, alternatives, value, handleClick }) => {
-  let label = emoji ? `${emoji} (${name})` : name;
+  const names = [
+    name,
+    ...(alternatives ? alternatives.map(alternative => alternative.name) : [])
+  ];
+  const [currentAlternative, setCurrentAlternative] = useState(0);
+  const currentName = names[currentAlternative];
+
+  let label = emoji ? `${emoji} (${currentName})` : currentName;
   label = quantity ? `${quantity} ${label}` : label;
+
+  const iterateNames = () => {
+    setCurrentAlternative(
+      currentAlternative === names.length - 1 ?
+        0 :
+        currentAlternative + 1
+    );
+  }
 
   return (
     <Item onClick={handleClick}>
       <Checkbox value={value} text={isOptional ? `{${label}}` : `${label}`} />
       {alternatives && alternatives.length > 0 && (
         <Action>
-          <IconButton edge="end" aria-label="comments">
+          <IconButton edge="end" aria-label="comments" handleClick={iterateNames}>
             <CommentIcon />
           </IconButton>
         </Action>
