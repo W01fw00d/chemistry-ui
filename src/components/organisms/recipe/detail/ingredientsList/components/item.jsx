fc03a6ae7b7f6af1a4199031df2525e4539b0e46
Gabriel Romay Machado <romay.gabriel@gmail.com>
2020-11-28 14:31:53 +0900

    Implement alternative names for ingredients

--- src/components/organisms/recipe/detail/ingredientsList/components/item.jsx
+++ src/components/organisms/recipe/detail/ingredientsList/components/item.jsx
@@ -2,30 +2,32 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { ListItemSecondaryAction as Action } from '@material-ui/core';
-import CommentIcon from '@material-ui/icons/Comment';
+import AutorenewIcon from '@material-ui/icons/Autorenew';
 
 import IconButton from '../../../../../atoms/buttons/icon.jsx';
 import Item from '../../../../list/item/index.jsx';
 import Checkbox from '../../../../list/item/checkbox.jsx';
 
-const Component = ({ name, emoji, quantity, isOptional, alternatives, value, handleClick }) => {
-  const names = [
-    name,
-    ...(alternatives ? alternatives.map(alternative => alternative.name) : [])
-  ];
-  const [currentAlternative, setCurrentAlternative] = useState(0);
-  const currentName = names[currentAlternative];
+const Component = ({
+  name,
+  emoji,
+  quantity,
+  isOptional,
+  alternatives,
+  value,
+  handleClick,
+  currentNameIndex,
+  setCurrentNameIndex,
+}) => {
+  const names = [name, ...(alternatives ? alternatives.map(alternative => alternative.name) : [])];
+  const currentName = names[currentNameIndex];
 
   let label = emoji ? `${emoji} (${currentName})` : currentName;
   label = quantity ? `${quantity} ${label}` : label;
 
   const iterateNames = () => {
-    setCurrentAlternative(
-      currentAlternative === names.length - 1 ?
-        0 :
-        currentAlternative + 1
-    );
-  }
+    setCurrentNameIndex(currentNameIndex === names.length - 1 ? 0 : currentNameIndex + 1);
+  };
 
   return (
     <Item onClick={handleClick}>
@@ -33,7 +35,7 @@ const Component = ({ name, emoji, quantity, isOptional, alternatives, value, han
       {alternatives && alternatives.length > 0 && (
         <Action>
           <IconButton edge="end" aria-label="comments" handleClick={iterateNames}>
-            <CommentIcon />
+            <AutorenewIcon />
           </IconButton>
         </Action>
       )}
